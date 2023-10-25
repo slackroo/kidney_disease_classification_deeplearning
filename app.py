f214a755b019d785c435ad296718e81a3cf77847
slackroo <sumanthomgowda@gmail.com>
2023-10-25 18:02:17 +0530

    changed docker mask-password to true

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def home():
 @app.route("/train",methods=['GET','POST'])
 @cross_origin()
 def trainRoute():
-    # os.system("python main.py")
-    os.system("dvc repro")
+    os.system("python main.py")
+    # os.system("dvc repro")
     return "Training done sucessfully"
 
 @app.route("/predict",methods=['POST'])
